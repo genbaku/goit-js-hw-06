a9a36ac354148ed4bd876bf81a3210156f2c4645
genbaku <140975610+genbaku@users.noreply.github.com>
2023-11-18 01:01:50 +0100

    123

--- task-1.js
+++ task-1.js
@@ -1,9 +1,29 @@
 "use strict"
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`
-}
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  }
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
--- task-2.js
+++ task-2.js
@@ -1,9 +1,24 @@
 "use strict"
-function getShippingMessage(country, price, deliveryFee) {
-  return `Shipping to ${country} will cost ${
-    price + deliveryFee
-  } credits`
+class Storage {
+  constructor(items) {
+    this._items = items;
+  }
+  getItems() {
+    return this._items;
+  }
+  addItem(newItem) {
+    this._items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const index = this._items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this._items.splice(index, 1);
+    }
+  }
 }
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
